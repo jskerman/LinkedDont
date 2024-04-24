2f26c677271c64e3083314e99d394b4d3c7967d1
Jack Skerman <jack.skerm@gmail.com>
2024-04-24 10:54:48 +0100

    change scrape

--- public/background.js
+++ public/background.js
@@ -2,14 +2,29 @@
 console.log("Service Worker Live");
 const linkedInAccountURL = "https://www.linkedin.com/in/";
 
+
 const scrapeProfile = () => {
-  // TODO: This sucks, pull relevant text data instead of all of it. 
-  const profileContainer = window.document.querySelector(
-    `.scaffold-layout__main`
-  )
-  const profile = profileContainer.textContent;
-  const cleanedProfile = profile.replace(/\s\s+/g, " ");
-  return cleanedProfile;
+  // TODO: this stuff needs to be in the function scope in extensions, should be able to pass in below
+  const NAME_PATH = "/html/body/div[6]/div[3]/div/div/div[2]/div/div/main/section[1]/div[2]/div[2]/div[1]/div[1]/span[1]/a/h1"
+  const TITLE_PATH = "/html/body/div[6]/div[3]/div/div/div[2]/div/div/main/section[1]/div[2]/div[2]/div[1]/div[2]"
+  const ABOUT_PATH = "/html/body/div[6]/div[3]/div/div/div[2]/div/div/main/section[3]/div[3]/div/div/div"
+  const CURRENT_EXPERIENCE_PATH = "/html/body/div[5]/div[3]/div/div/div[2]/div/div/main/section[5]/div[3]/ul/li[1]/div/div[2]"
+  const getElementByXpath = (path) => document.evaluate(path, document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue;
+  // </stuff>
+
+  const name = getElementByXpath(NAME_PATH) 
+  const title = getElementByXpath(TITLE_PATH)
+  const about = getElementByXpath(ABOUT_PATH)
+  const currentExperience = getElementByXpath(CURRENT_EXPERIENCE_PATH)
+  console.log(NAME_PATH, name)
+  const profile = `
+  NAME: ${name ? name.textContent : "N/A"}
+  TITLE: ${title ? title.textContent : "N/A"}
+  ABOUT: ${about ? about.textContent.replaceAll('\n', ' ') : "N/A"}
+  CURRENT EXPERIENCE: ${currentExperience ? currentExperience.textContent.replace(/\s\s+/g, " "): "N/A"}
+  `
+
+  return profile;
 };
 
 
